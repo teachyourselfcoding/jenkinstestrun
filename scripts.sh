0c232f32456f5e06752223080131dc59fdb9bfc4
teachyourselfcoding <60517710+teachyourselfcoding@users.noreply.github.com>
2021-04-16 03:11:58 +0000

    Update scripts.sh

--- scripts.sh
+++ scripts.sh
@@ -1,3 +1,30 @@
-cd src/main/java/
-javac Duke.java
-java Duke
+pipeline {
+    agent any
+    stages {
+        stage('build') {
+            stages{
+                stage('First build'){
+                    steps {
+                        sh 'echo "Hello World"'
+                        sh 'python --version'
+                    }
+                }
+                stage('Second build'){
+                    steps {
+                        sh 'echo "This is my second build"'
+                    }
+                }                
+            }
+            
+        }
+
+        stage('run') {
+            steps {
+                script{
+                    sh "scripts.sh"
+                }
+
+            }
+        }
+    }
+}
